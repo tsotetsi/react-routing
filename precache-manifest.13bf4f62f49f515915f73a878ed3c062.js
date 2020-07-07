d8e6557f15666b609345dc21af099bb3812ab6fb
Thapelo Tsotetsi <me@tsotetsithapelo.co.za>
2020-07-07 12:23:20 +0200

    Updates

---
+++ precache-manifest.13bf4f62f49f515915f73a878ed3c062.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa7618165d78855cde2d8f31a840e579",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd3255cd4721419757ed",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0826e766a784a2a85a20",
+    "url": "/static/js/2.63698a1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.63698a1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd3255cd4721419757ed",
+    "url": "/static/js/main.14fc9f9a.chunk.js"
+  },
+  {
+    "revision": "b80c6f8714bc1e133bbf",
+    "url": "/static/js/runtime-main.4a0803b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
